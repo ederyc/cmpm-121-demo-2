90561d232067af2c7e5c36cb49d877fbd84c7629
ederyc <edyepezc@ucsc.edu>
2024-10-16 12:26:01 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -21,12 +21,19 @@ clearButton.textContent = 'Clear';
 app.appendChild(clearButton);
 
 let isDrawing = false;
-let x = 0;
-let y = 0;
+let points: [number, number] [] [] = [];
+let currentPath: [number, number] [] = [];
+
+const drawingChanged = new Event("drawing-changed");
+
+function addPoint(x: number, y: number) {
+    currentPath.push([x, y]);
+    canvas.dispatchEvent(drawingChanged);
+}
 
 canvas.addEventListener('mousedown', (event) => {
-    x = event.offsetX;
-    y = event.offsetY;
+    currentPath = [];
+    addPoint(event.offsetX, event.offsetY);
     isDrawing = true;
   });
   
@@ -34,33 +41,59 @@ canvas.addEventListener('mousedown', (event) => {
   globalThis.addEventListener('mouseup', () => {
     if (isDrawing) {
       isDrawing = false;
+      if (currentPath.length > 0) {
+        points.push(currentPath);
+      }
     }
   });
 
   canvas.addEventListener('mousemove', (event) => {
     if (isDrawing) {
-      drawLine(x, y, event.offsetX, event.offsetY);
-      x = event.offsetX;
-      y = event.offsetY;
+        addPoint(event.offsetX, event.offsetY);
     }
   });
-  
-  //function to draw
-  function drawLine(x1: number, y1: number, x2: number, y2: number) {
-    if (!context) return; //this line included to get pass a 'context possibly null error' that was being thrown
-    context.beginPath();
+
+  //observer for drawing-changed
+  canvas.addEventListener('drawing-changed', redrawCanvas);
+
+  function redrawCanvas() {
+    if (!context) return;
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.lineWidth = 2;
     context.strokeStyle = 'blue';
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();
-    context.closePath();
+
+    for (const path of points) {
+        context.beginPath();
+        context.moveTo(path[0][0], path[0][1]);
+        for (const point of path) {
+            context.lineTo(point[0], point[1]);
+        }
+        context.stroke();
+        context.closePath();
+    }
+
+    /*this part needed to be added because before when I would
+    try to draw lines, they would only appear after I had released the mouse
+    and when I had begun to start the other line
+    this way, they are now appearing as you draw them*/
+    if (currentPath.length > 0) {
+        context.beginPath();
+        context.moveTo(currentPath[0][0], currentPath[0][1]);
+        for (const point of currentPath) {
+            context.lineTo(point[0], point[1]);
+        }
+        context.stroke();
+        context.closePath();
+    }
   }
+  
+
   
   //clearButton
   clearButton.addEventListener('click', () => {
     if (!context) return; //this line included to get pass a 'context possibly null error' that was being thrown
     context.clearRect(0, 0, canvas.width, canvas.height);
+    points = [];
   });
 
 
