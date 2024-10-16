c2578d83b5caf2138da79de2515da54c3089e97d
ederyc <edyepezc@ucsc.edu>
2024-10-16 11:27:36 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,11 @@ const APP_NAME = "Eder's Cool App";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+
+const canvas = document.createElement('canvas');
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = 'canvas';
+app.appendChild(canvas);
+
